27f824e2cbea6119df0506f9dd252e072fb7c109
ruhan-2908 <ruhankb29@gmail.com>
2025-10-24 23:15:18 +0530

    Basic project setup

---
+++ src/main/java/com/E_Commerce/RuhKart/RuhKartApplication.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.RuhKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuhKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/E_Commerce/RuhKart/RuhKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce.RuhKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuhKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
