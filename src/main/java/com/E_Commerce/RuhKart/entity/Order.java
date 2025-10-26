bdddb106e187821948f558152b1ce024e400ad65
ruhan-2908 <ruhankb29@gmail.com>
2025-10-26 17:53:51 +0530

    Add Order and OrderItem

---
+++ src/main/java/com/E_Commerce/RuhKart/entity/Order.java
@@ -0,0 +1,72 @@
+package com.E_Commerce.RuhKart.entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.engine.internal.Cascade;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems;
+    private Double totalItemsAmount;
+    private Double taxAmount;
+    private Double totalAmount;
+    private String status;
+    private String orderNo;
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Double getTaxAmount() {
+        return taxAmount;
+    }
+
+    public void setTaxAmount(Double taxAmount) {
+        this.taxAmount = taxAmount;
+    }
+
+    public Double getTotalItemsAmount() {
+        return totalItemsAmount;
+    }
+
+    public void setTotalItemsAmount(Double totalItemsAmount) {
+        this.totalItemsAmount = totalItemsAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+}
---
+++ src/main/java/com/E_Commerce/RuhKart/entity/OrderItem.java
@@ -0,0 +1,70 @@
+package com.E_Commerce.RuhKart.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer quantity;
+    private String image;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Product product;
+
+    public OrderItem(String name, Integer quantity, String image, Double price, Product product) {
+        super();
+        this.name = name;
+        this.quantity = quantity;
+        this.image = image;
+        this.price = price;
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
